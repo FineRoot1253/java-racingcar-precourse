3bde276662577e7740c136969daab45ae0e53239
FineRoot1253 <dev.fineroot1253@gmail.com>
2022-10-05 12:51:40 +0900

    feat(Constants): add Constants Class
    Add Constants Class for utility Constants
    
     - add PROGRESS_PARTICLE for representing progress of car
     - add COMMON_ERROR_HEADER for representing exception message
     - add ERROR_CAR_NAME_SIZE_OVER for representing exception message
     - add ERROR_CAR_DUPLICATE for representing exception message

---
+++ src/main/java/racingcar/common/Constants.java
@@ -0,0 +1,13 @@
+package racingcar.common;
+
+public class Constants {
+    private Constants() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static final String PROGRESS_PARTICLE = "-";
+
+    public static final String COMMON_ERROR_HEADER = "[ERROR] ";
+    public static final String ERROR_CAR_NAME_SIZE_OVER = "은(는) 이름이 너무 깁니다.";
+    public static final String ERROR_CAR_DUPLICATE = "은(는) 중복입니다.";
+}
